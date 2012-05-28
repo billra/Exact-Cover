c52359dedace2e42b125cc179b8400c0baaf0fda
Bill Ola Rasmussen <billra@gmail.com>
2012-05-28 20:59:37 +0200

    dlx work

--- dlx.cpp
+++ dlx.cpp
@@ -1,6 +1,52 @@
-// dlx solver implementation
+// classic dancing links solver implementation
 // Bill Ola Rasmussen
 #include "dlx.h"
+
+// Knuth's Algorithm X  from http://www-cs-faculty.stanford.edu/~uno/papers/dancing-color.ps.gz
+// -------------------
+// If A is empty, the problem is solved; terminate successfully.
+// Otherwise choose a column, c (deterministically).
+// Choose a row, r, such that A[r,c] = 1 (nondeterministically).
+// Include r in the partial solution.
+// For each j such that A[r,j] = 1,
+//     delete column j from matrix A;
+//         for each i such that A[i,j] = 1,
+//             delete row i from matrix A.
+// Repeat this algorithm recursively on the reduced matrix A.
+//
+// deterministically: choose column with fewest 1's
+
+// Algorithm X dlx data structure (from same paper)
+// ------------------------------
+// One good way to implement algorithm X is to represent each 1 in the
+// matrix A as a data object x with five fields L[x], R[x], U[x], D[x], C[x]. Rows of the matrix
+// are doubly linked as circular lists via the L and R fields (“left” and “right”); columns are
+// doubly linked as circular lists via the U and D fields (“up” and “down”). Each column
+// list also includes a special data object called its list header.
+//     The list headers are part of a larger object called a column object. Each column object
+// y contains the fields L[y], R[y], U [y], D[y], and C[y] of a data object and two additional
+// fields, S[y] (“size”) and N [y] (“name”); the size is the number of 1s in the column, and the
+// name is a symbolic identifier for printing the answers. The C field of each object points
+// to the column object at the head of the relevant column.
+//     The L and R fields of the list headers link together all columns that still need to be
+// covered. This circular list also includes a special column object called the root, h, which
+// serves as a master header for all the active headers. The fields U[h], D[h], C[h], S[h], and
+// N[h] are not used.
+
+// Generalized Exact Cover (from same paper)
+// -----------------------
+// We can handle this extra complication by generalizing the exact cover problem. Instead
+// of requiring all columns of a given 0-1 matrix to be covered by disjoint rows, we
+// will distinguish two kinds of columns: primary and secondary. The generalized problem
+// asks for a set of rows that covers every primary column exactly once and every secondary
+// column at most once.
+// 
+// Fortunately, we can solve the generalized cover problem by using almost the same
+// algorithm as before. The only difference is that we initialize the data structure by making
+// a circular list of the column headers for the primary columns only. The header for each
+// secondary column should have L and R fields that simply point to itself. The remainder
+// of the algorithm proceeds exactly as before, so we will still call it algorithm DLX.
+
 
 DLX::DLX(int pc, int sc):Solver(pc,sc)
 {
--- readme.txt
+++ readme.txt
@@ -1,6 +1,15 @@
 Code to create and solve exact cover problems.
 
-usage:
+----------
+Motivation
+----------
+"Due to its NP-completeness, any problem in NP can be reduced to exact cover problems, which then can be solved with techniques such as Dancing Links. However, for some well known problems, the reduction is particularly direct. For instance, the problem of tiling a board with pentominoes, and solving Sudoku can both be viewed as exact cover problems." from http://en.wikipedia.org/wiki/Exact_cover#Noteworthy_examples
+
+So, an exact cover solver can be used to solve any problem in NP.
+
+-----
+Usage
+-----
 CreateQueensData {size} - create exact cover problem data for n queens board size
 CreateQueensData - no parameter to run unit tests on creation code
 CreateQueensData {size} | solve > soln.txt - stream problem into solver and write solution to file (todo!)
