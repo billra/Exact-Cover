49f0982e3a61bcb04a7472bbd04e34678f887712
Bill Ola Rasmussen <billra@gmail.com>
2015-06-05 17:18:46 +0200

    indexing type and range description

--- acx.h
+++ acx.h
@@ -5,6 +5,10 @@
 #include "Solver.h"
 
 class ACX : public Solver {
+	// C++11 <cstdint> types:
+	//	uint8_t		0 to           255	1 byte
+	//	int16_t		0 to        65,535	2 bytes
+	//	int32_t		0 to 4,294,967,295	4 bytes, same as Microsoft unsigned int
 	using TI = unsigned int; // index type
 	bool _show;
 	std::function<void(Event)> _notify;
